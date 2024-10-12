c8dedae5219f92f207bfbaf12dbe8593cb9615a3
raito889 <jonathanknjghtz@gmail.com>
2024-10-12 17:45:17 +0700

    update event 2007

---
+++ server/jxser/server1/script/activitysys/config/2024/head.lua
@@ -0,0 +1,9 @@
+Include("\\script\\activitysys\\activity.lua")
+pActivity = ActivityClass:new()
+pActivity.nId = 2024
+pActivity.szName = "mengjiang2007"
+pActivity.nStartDate = 202404020000
+pActivity.nEndDate = 202505010000
+pActivity.szDescription = "春节刚到，万物变⑻，又一个植树节，农民处处忙，希望各位大侠帮一息忙"
+pActivity.nGroupId = 20
+pActivity.nVersion = 1
